ee94bd9f0aefbaa737c7b969f6511164676866f2
jcperkins <jcperkins12@gmail.com>
2021-04-22 16:16:56 -0500

    add a monitor batch file to the queue folder

--- rc_backup_sample/run-all-and-monitor-15min.bat
+++ rc_backup_sample/run-all-and-monitor-15min.bat
@@ -2,6 +2,9 @@
 @REM Sample batch file to automate pushing local changes to a directory
 @REM for each 'src_path' and 'dest_path' use the following commands. It leverages
 @REM the utility jobs in the home directory of the robocopy-jobs folder.
+@REM
+@REM The /MOT:15 option is added to each robocopy execution to check for updates
+@REM every 15 minutes
 
 set maxerror=0
 FOR %%j IN (*.rcj) DO (
@@ -14,7 +17,7 @@ EXIT /B %maxerror%
 @REM can be contained within the function
 :run_rc_job
 @REM run the robocopy job
-robocopy /JOB:%~1
+robocopy /JOB:%~1 /MOT:15
 @REM If you want do change the error code reported by robocopy, do so
 @REM here with some thing like this.
 @REM robocopy /JOB:%~1 /L ^& IF %ERRORLEVEL% LEQ 1 EXIT /B 0
